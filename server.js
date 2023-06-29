a26c36773c3e38bbe5a1941129e3da17a6e3c638
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2023-06-29 12:29:47 -0400

    Created the server.js app.

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const db = require('./database/db');
+const PORT="8080"
+const app =express();
+
+app.use("/api",require("/.apis"))
+
+const serverRun=()=>{
+    app.listen(PORT,()=>{
+        console.log("Live on port:"+PORT);
+    })
+};
+
+const syncServer=()=>db.sync();
+
+syncServer();
+serverRun()
+
+module.exports=app;
