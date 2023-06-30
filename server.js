c280f357d9e9b0de7098342f3e19a67850ac2456
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-06-29 22:15:41 -0400

    permissions for servers to talk to one another added

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const db = require('./database/db');
 const PORT="8080"
-const app =express();
+const app = express();
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3002');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use("/api",require("./apis"))
+//app.use(cors());
 
 const serverRun=()=>{
     app.listen(PORT,()=>{
