6ef1684ebbb0dc765e4b1f0dcf2caf525c504bf7
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-02 12:47:09 -0400

    adding student to a campus method made

--- apis/campuses.js
+++ apis/campuses.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {campus} = require("../database/models");
+const {campus, student} = require("../database/models");
 router.use(require('express').json())
 //Root here is localhost:8080/api/campuses
 router.get("/", async (req, res, next) => {
@@ -18,7 +18,7 @@ router.get("/", async (req, res, next) => {
     }
 })
 
-router.post('/',async(req,res,next)=>{
+router.post('/', async (req,res,next)=>{
     try{
         //deconstruning the data passed from the form
         const {name,image,address,description}= req.body;
@@ -29,7 +29,7 @@ router.post('/',async(req,res,next)=>{
     }
 });
 
-router.get("/:campusid",async (req,res,next)=>{
+router.get("/:campusid", async (req,res,next)=>{
     try{
         const campusFound= await campus.findByPk(req.params.campusid);
         campusFound?res.status(200).json(campusFound):res.status(400).send("Campus not found") //typo in res message should be Campus instead of Student
@@ -37,4 +37,32 @@ router.get("/:campusid",async (req,res,next)=>{
         next(error)
     }
 });
- module.exports = router;
+
+router.post('/:campusid', async (req,res,next) => {
+    const id = req.params.campusid
+    const { firstName, lastName, address, image, email, gpa } = req.body
+    try {
+        // Look for a 'campus' record in the database by its primary key 'id'
+        const campusfound = await campus.findByPk(id)
+        
+        // If the campus is not found, send a 400 status response with an error message
+        if(!campusfound){
+            return res.status(400).send('Campus not found')
+        }
+
+        // If the campus is found, create a new 'student' record in the database,
+        // using the data sent in the request body and set 'campusId' to 'id'
+        const newstudent = student.create({firstName, lastName, address, image, email, gpa, campusId:id})
+
+        // If the new student record was successfully created,
+        // send a 200 status response with the new student's data in JSON format.
+        // Otherwise, send a 400 status response with an error message.
+        campusfound? res.status(200).json(newstudent): res.status(400).send('Student added to campus')
+    } catch (error) {
+       next(error)
+    }
+} )
+
+
+
+module.exports = router;
