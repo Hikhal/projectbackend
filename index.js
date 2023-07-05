6814c9e439674d7a2c6ed5f3973f89f20a484904
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-05 18:45:57 -0400

    commented out db connectivity code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const db = require('./database/db');
+// const db = require('./database/db');
 const PORT=8080
 const app = express();
 
@@ -30,16 +30,16 @@ app.use((error, req, res, next) => {
 
 })
 
-const serverRun=()=>{
-    app.listen(PORT,()=>{
-        console.log("Live on port:"+PORT);
-    })
-};
+// const serverRun=()=>{
+//     app.listen(PORT,()=>{
+//         console.log("Live on port:"+PORT);
+//     })
+// };
 
-const syncServer=()=>db.sync();
+// const syncServer=()=>db.sync();
 
-syncServer();
-serverRun()
+// syncServer();
+// serverRun()
 
 module.exports=app;
 
