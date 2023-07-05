a3dcfc595481435386f6b40b611e6a93ba005b92
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-05 18:31:47 -0400

    debug vercel error

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const db = require('./database/db');
+//const db = require('./database/db');
 const bodyParser = require('body-parser');
 const PORT="8080"
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   next();
+// });
 
 // Use body-parser middleware
 app.use(bodyParser.json());
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-app.use("/api",require("./apis"))
+// app.use("/api",require("./apis"))
 //app.use(cors());
 
 app.get('/', (req, res) => {
@@ -31,16 +31,16 @@ app.use((error, req, res, next) => {
 
 })
 
-const serverRun=()=>{
-    app.listen(PORT,()=>{
-        console.log("Live on port:"+PORT);
-    })
-};
+// const serverRun=()=>{
+//     app.listen(PORT,()=>{
+//         console.log("Live on port:"+PORT);
+//     })
+// };
 
-const syncServer=()=>db.sync({force:true});
+// const syncServer=()=>db.sync({force:true});
 
-syncServer();
-serverRun()
+// syncServer();
+// serverRun()
 
 module.exports=app;
 
